27393e3a7236ba9aafa244f06fcea1e9daca6934
guilherme.carmo <guilherme.carmo@take.net>
2024-06-06 10:06:01 -0300

    change otimize

--- script.js
+++ script.js
@@ -2,7 +2,6 @@ document.getElementById('calcular').addEventListener('click', function() {
     const textoVaga = document.getElementById('vaga').value.toLowerCase();
     const textoCurriculo = document.getElementById('curriculo').value.toLowerCase();
 
-    // Lista de stopwords em português
     const stopWords = [
         "a", "à", "ao", "aos", "as", "às", "de", "da", "das", "do", "dos", "acoes", "apoiar", "quanto", "relacionadas", "nestas", "tecnologias",
         "e", "em", "na", "nas", "no", "nos", "o", "os", "um", "uma", "umas", "uns", "voce","qualificacoes", "companhia", "sua", "dentro",
@@ -16,16 +15,12 @@ document.getElementById('calcular').addEventListener('click', function() {
         "trata", "las", "completo", "final", "pontos", "boas", "auto", "enviar", "criar", "basico", "desejavel", "superior", "ocorra", "incluindo", "todas"
     ];
 
-    // Função para normalizar texto removendo acentos e pontuações
     function normalize(text) {
         return text.normalize('NFD').replace(/[\u0300-\u036f]/g, '').replace(/[^\w\s]/gi, '');
     }
 
-    // Função para tokenização e remoção de stopwords
     function tokenizeAndFilter(text) {
-        const doc = nlp(text);
-        const tokens = doc.terms().out('array');
-        return tokens
+        return text.split(/\s+/)
             .map(word => normalize(word.toLowerCase()))
             .filter(word => !stopWords.includes(word));
     }
@@ -33,44 +28,31 @@ document.getElementById('calcular').addEventListener('click', function() {
     const filteredTokensVaga = tokenizeAndFilter(textoVaga);
     const filteredTokensCurriculo = tokenizeAndFilter(textoCurriculo);
 
-    // Função para calcular TF-IDF
-    function calculateTFIDF(docs) {
-        const tfidf = {};
-        const docCount = docs.length;
-
-        // Contagem de termos
-        docs.forEach((doc, docIndex) => {
-            const terms = doc.split(/\s+/);
-            const termFreq = {};
-
-            terms.forEach(term => {
-                if (!termFreq[term]) {
-                    termFreq[term] = 0;
-                }
-                termFreq[term]++;
-            });
-
-            Object.keys(termFreq).forEach(term => {
-                const tf = termFreq[term] / terms.length;
-
-                if (!tfidf[term]) {
-                    tfidf[term] = { tf: [], idf: 0 };
-                }
-                tfidf[term].tf[docIndex] = tf;
-            });
-        });
-
-        // Calculando IDF
-        Object.keys(tfidf).forEach(term => {
-            const df = tfidf[term].tf.filter(f => f > 0).length;
-            tfidf[term].idf = Math.log(docCount / (df + 1)) + 1; // Suavização
-        });
-
-        return tfidf;
-    }
-
+    const tfidf = {};
     const docs = [filteredTokensVaga.join(' '), filteredTokensCurriculo.join(' ')];
-    const tfidf = calculateTFIDF(docs);
+    const docCount = docs.length;
+
+    docs.forEach((doc, docIndex) => {
+        const terms = doc.split(/\s+/);
+        const termFreq = {};
+
+        terms.forEach(term => {
+            termFreq[term] = (termFreq[term] || 0) + 1;
+        });
+
+        Object.keys(termFreq).forEach(term => {
+            const tf = termFreq[term] / terms.length;
+            if (!tfidf[term]) {
+                tfidf[term] = { tf: Array(docCount).fill(0), idf: 0 };
+            }
+            tfidf[term].tf[docIndex] = tf;
+        });
+    });
+
+    Object.keys(tfidf).forEach(term => {
+        const df = tfidf[term].tf.filter(f => f > 0).length;
+        tfidf[term].idf = Math.log(docCount / (df + 1)) + 1;
+    });
 
     let commonTerms = [];
     Object.keys(tfidf).forEach(term => {
@@ -86,7 +68,6 @@ document.getElementById('calcular').addEventListener('click', function() {
 
     document.getElementById('resultado').innerHTML = resultado;
 
-    // Sugestão de termos a serem incluídos no currículo
     const termosSugeridos = filteredTokensVaga.filter(term => !filteredTokensCurriculo.includes(term));
     let sugestao = `Sugestões de termos a serem incluídos no currículo:<br> ${[...new Set(termosSugeridos)].join(', ')}`;
 
